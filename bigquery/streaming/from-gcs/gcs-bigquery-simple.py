81644c1600c0799b2cb5b8dc9df58ee1fdf461d1
ramon <ramon4mm@gmail.com>
2020-11-18 14:44:02 +0000

    added more comments

--- bigquery/streaming/from-gcs/gcs-bigquery-simple.py
+++ bigquery/streaming/from-gcs/gcs-bigquery-simple.py
@@ -6,32 +6,19 @@ import logging
 import argparse
 import datetime
 
-#Fill accordingly, we are using a table-id sport_simple for future reference
+# Fill accordingly, we are using a table-id sport_simple for future reference
 schema = 'ex1:STRING, ex2:String, ex3:Int'
 table= '<dataset_id>.sport_simple'
 gcs_file = "gs://<MyBucket>/sport-data.txt"
 
-class AddTimestamps(beam.DoFn):
-    def process(self, element, publish_time=beam.DoFn.TimestampParam):
-        """Processes each incoming element by extracting the Pub/Sub
-        message and its publish timestamp into a dictionary. `publish_time`
-        defaults to the publish timestamp returned by the Pub/Sub server. It
-        is bound to each element by Beam at runtime.
-        """
-
-        yield {
-            "message_body": element.decode("utf-8"),
-            "publish_time": datetime.datetime.utcfromtimestamp(
-                float(publish_time)
-            ).strftime("%Y-%m-%d %H:%M:%S.%f"),
-        }
-
+# We need to define a transformation step to clean the incoming data
+# Given that the data has the format 'Brand Equipment, cost$' we use the following transformation: 
 class Transform_text(beam.DoFn):
   def process(self,element):
     element = element.strip()
     splitted = element.split(',')
     full_splitted = splitted[0].split(' ')
-    #TODO transform incoming gcs data into bq writable
+    # Transform incoming gcs data into bq writable, meaning a dictionary
     yield {
       "Brand": full_splitted[0],
       "Equipment": full_splitted[1],
@@ -44,15 +31,16 @@ def main(argv=None):
   parser.add_argument("--input_file", default=gcs_file)
   parser.add_argument("--output_table", default=table)
   args, beam_args = parser.parse_known_args(argv)
-#save_main_session needs to be set to true in order to avoid datetime not found issues
+  # Uncomment the service account email parameter if you wish to use a custom service account
   p = beam.Pipeline(argv=beam_args,options=PipelineOptions(save_main_session=True,
 region='us-central1'))#, service_account_email='email'))
 
   (p
-      | 'ReadData' >> beam.io.textio.ReadFromText(args.input_file) #AFAIK this creates a PCollection of lines
-      | "Transform text" >> beam.ParDo(Transform_text())
+      | 'ReadData' >> beam.io.textio.ReadFromText(args.input_file) # This creates a PCollection of lines
+      | "Transform text" >> beam.ParDo(Transform_text()) # This transform the lines into BQ writtable
       | 'WriteToBigQuery' >> beam.io.WriteToBigQuery(args.output_table, schema=schema, write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND)
-  )
+  ) # This writes the data to BQ
+
   result = p.run()
   result.wait_until_finish()
 
